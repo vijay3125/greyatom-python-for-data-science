4e48a016f424cf8391f378116b584e38f3bf058a
vijay3125 <60693448+vijay3125@users.noreply.github.com>
2020-05-15 11:45:00 +0530

    Update code.py

--- vijay3125/code.py
+++ vijay3125/code.py
@@ -4,10 +4,11 @@
 # Create the lists 
 class_1 = ["Geoffrey Hinton","Andrew Ng","Sebastian Raschka","Yoshua Bengio"]
 class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
-# Concatenate both the strings
 
+# Concatenate both the strings
 new_class = class_1 + class_2
 print(new_class)
+
 # Append the list
 new_class.append('Peter Warden')
 
@@ -16,6 +17,7 @@ print(new_class)
 
 # Remove the element from the list
 new_class.remove('Carla Gentry')
+
 # Print the list
 print(new_class)
 
@@ -23,25 +25,20 @@ print(new_class)
 # Create the Dictionary
 courses = {'Math':65,'English':70,'History':80,'French':70,'Science':60}
 
-
-
-# Slice the dict and stores  the all subjects marks in variable
-
-
 # Store the all the subject in one variable `Total`
 total = sum(courses.values()) 
+
 # Print the total
 print(total)
+
 # Insert percentage formula
 percentage = (total / 500) * 100
+
 # Print the percentage
 print(percentage)
 
-
-
 # Create the Dictionary
 mathematics = {'Geoffrey Hinton':78,'Andrew Ng\t ':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
-
 topper = max(mathematics,key = mathematics.get)
 print(topper)
 
@@ -52,16 +49,21 @@ print(topper)
 # Create variable first_name 
 first_name = topper[0]
 print(first_name)
+
 # Create variable Last_name and store last two element in the list
 last_name = topper[1]
 print(last_name,sep=' ')
+
 # Concatenate the string
 full_name = last_name +' '+ first_name  
+
 # print the full_name
 print(full_name)
+
 # print the name in upper case
 certificate_name = full_name.upper()
 print('certificate_name is ',certificate_name)
+
 # Code ends here
 
 
